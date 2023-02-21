83e61b69327de9397003131ed0a46840d285ff99
phamduy0165 <75572336+phamduy0165@users.noreply.github.com>
2023-02-21 07:58:55 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useMemo } from 'react';
+import './App.css';
+
+export default function App() {
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const [products, setProducts] = useState([]);
+
+  const total = useMemo(() => {
+    const result = products.reduce((acc, ele) => {
+      console.log('Tính toán lại ...');
+      return acc + ele.price;
+    }, 0);
+    return result;
+  }, [products]);
+
+
+  const onAddProduct = () => setProducts([...products, { name, price: Number(price) }]);
+
+  return (
+    <div className='container'>
+      <input
+        placeholder='Enter name...'
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <br />
+      <input
+        placeholder='Enter price...'
+        value={price}
+        onChange={e => setPrice(e.target.value)}
+      />
+      <div onClick={onAddProduct}>Add product</div>
+      <div>Total: {total}</div>
+      <ul>
+        {products.map((e, i) => <li key={i}>{e.name} - {e.price}</li>)}
+      </ul>
+    </div>
+  );
+}
